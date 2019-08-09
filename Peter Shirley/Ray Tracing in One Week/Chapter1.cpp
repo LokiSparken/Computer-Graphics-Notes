d0ba4619001f56c295746f22f0da150f6e26df2d
LokiSparken <sparken@qq.com>
2019-08-10 02:24:33 +0800

    Chapter1 ok

---
+++ Peter Shirley/Ray Tracing in One Week/Chapter1.cpp
@@ -0,0 +1,31 @@
+// Output an image in ppm file
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream output;    // 创建ofstream对象
+    output.open("chapter1.ppm");    // 关联文本文件，输出后为测试效果对文件名有所改动
+
+    // 按nx*ny像素输出每个像素的RGB到P3格式的ppm文件中
+    int nx = 400, ny = 200;
+    output << "P3\n" 
+           << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            float r = float(i) / float(nx);
+            float g = 0.2;
+            float b = float(j) / float(ny);
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            output << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+
+    output.close(); // 使用完文本文件后关闭
+    return 0;
+}
---
+++ Peter Shirley/Ray Tracing in One Week/note - Ray Tracing in One Week.md
@@ -0,0 +1,29 @@
+> # Peter Shirley - Ray Tracing in One Week
+# Chapter 0: Overview
+
+# Chapter 1: Output an image 
+* .ppm格式：P3 nx*ny 各像素点的RGB
+  * 可以直接看颜色效果欸好神奇ovo
+
+# Chapter 2: The vec3 class
+
+# Word Box
+## Chapter 0: Overview
+* staple adj. 主要的，基本的 n. 订书钉，基本食物 v. 用订书钉装订
+* preach v. 宣讲，说教
+
+## Chapter 1: Output an image
+
+## Chapter 2: The vec3 class
+* transparency n. 幻灯片，透明性，显而易见
+
+
+<i class="fa fa-star"></i>
+<!-- 使用FontAwesome -->
+<head> 
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js"></script> 
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/v4-shims.js"></script> 
+</head> 
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+
+<!-- Sample use: <i class="fa fa-star"></i> -->
